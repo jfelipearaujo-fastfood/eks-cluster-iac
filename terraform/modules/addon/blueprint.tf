fb9704abcc2ff0b65141ddd0568598ba5944da97
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-03-10 01:41:16 -0300

    feat: :sparkles: add EBS

--- terraform/modules/addon/blueprint.tf
+++ terraform/modules/addon/blueprint.tf
@@ -7,9 +7,38 @@ module "eks_blueprints_addons" {
   cluster_endpoint  = var.cluster_endpoint
   oidc_provider_arn = var.oidc_provider_arn
 
-  enable_metrics_server = true
-
+  enable_metrics_server               = true
+  enable_aws_efs_csi_driver           = true
   enable_aws_load_balancer_controller = true
+
+  eks_addons = {
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+    }
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      # Specify the VPC CNI addon should be deployed before compute to ensure
+      # the addon is configured before data plane compute resources are created
+      # See README for further details
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+      before_compute           = true
+      most_recent              = true
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+  }
+
   aws_load_balancer_controller = {
     set = [
       {
@@ -21,5 +50,38 @@ module "eks_blueprints_addons" {
         value = 1
       },
     ]
+  }
+}
+
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.37.1"
+
+  role_name_prefix = "${var.cluster_name}-ebs-csi-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.37.1"
+
+  role_name_prefix = "${var.cluster_name}-vpc-cni-"
+
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
   }
 }
--- terraform/modules/cluster/eks.tf
+++ terraform/modules/cluster/eks.tf
@@ -10,18 +10,6 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
